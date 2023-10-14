b12cbaa3d2bb9856a4443dfddb189bd0857ef69c
nggakbisacoding <fajarwahyunugroho0802@mail.ugm.ac.id>
2023-10-15 06:19:10 +0700

    Initial Commit

--- app/src/main/java/com/apps/hotelku/SplashActivity.kt
+++ app/src/main/java/com/apps/hotelku/SplashActivity.kt
@@ -6,19 +6,23 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import com.apps.hotelku.databinding.ActivityDetailBinding
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000) // Ini menunda splash screen selama 3 detik (milidetik)
-
+        with(binding) {
+            Handler().postDelayed({
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000) // Ini menunda splash screen selama 3 detik (milidetik)
+        }
     }
-
 }
